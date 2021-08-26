477658d50147f8ad92cfd31f88d13a34993ad760
Michelle <clairsin@gmail.com>
2021-08-25 23:52:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const names = ["Sinclair", "Michelle", "Tsu", "Peter"];
+const eventName = "birthday"
+
+function writeCards(listOfNames, nameOfEvent) {
+  const birthdayThankYouMessages = []
+
+  for (let i = 0; i < listOfNames.length; i++) {
+    console.log (`Thank you, ${listOfNames[i]}, for the wonderful ${nameOfEvent} gift!`);
+    birthdayThankYouMessages.push(`Thank you, ${listOfNames[i]}, for the wonderful ${nameOfEvent} gift!`);
+  }
+
+  return birthdayThankYouMessages;  
+}
+
+writeCards(names, eventName);
+
+
+
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log (num--);
+  }
+}
+countDown(21);
